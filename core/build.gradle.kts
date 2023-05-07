2f9d7e7cd39398075b5c1f1f1943f916acd8bd05
HoYeon Lee <comforest@naver.com>
2023-05-07 14:32:32 +0900

    feat: 모듈 기본 골격 추가

---
+++ core/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    api(project(":port-in"))
+    implementation(project(":port-out"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "BoardGameRating"
-
+include(
+    "domain",
+    "core",
+    "port-in",
+    "port-out",
+    "adapter-in:web",
+    "adapter-out:rdb",
+)
