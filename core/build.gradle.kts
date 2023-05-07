9e0bccbe9c62f5a7c6f82d2d5a25cc56f8805cf1
HoYeon Lee <comforest@naver.com>
2023-05-07 17:52:05 +0900

    feat: core 모듈 Spring 세팅

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,11 @@
+
 dependencies {
+    val springVersion by properties
+
     api(project(":port-in"))
-    implementation(project(":port-out"))
+    implementation(project(":adapter-out:rdb"))
+
+    implementation("org.springframework.boot:spring-boot-starter:$springVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
 }
