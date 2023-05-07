f737f42ca076490c36e5815cc1ea525077dee5f6
HoYeon Lee <comforest@naver.com>
2023-05-07 15:28:15 +0900

    feat: Adpater-Out-RDB 모듈

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("kapt") version "1.7.22"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+
+    id("io.spring.dependency-management") version "1.1.0"
+
+    id("org.springframework.boot")
 }
 
 repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,19 @@
 rootProject.name = "BoardGameRating"
+
+pluginManagement {
+    val springVersion: String by settings
+    val kotlinPluginVersion: String by settings
+    
+    plugins {
+        id("org.springframework.boot") version springVersion
+        
+        kotlin("plugin.spring") version kotlinPluginVersion
+        kotlin("plugin.jpa") version kotlinPluginVersion
+        kotlin("plugin.allopen") version kotlinPluginVersion
+        kotlin("plugin.noarg") version kotlinPluginVersion
+    }
+}
+
 include(
     "domain",
     "core",
