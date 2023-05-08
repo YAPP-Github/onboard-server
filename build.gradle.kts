af3960ed6e846a97522f4fe76040bbcc8106d339
HoYeon Lee <comforest@naver.com>
2023-05-08 19:12:21 +0900

    test: TokenPolicy

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ subprojects {
 
         testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
         testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+        testImplementation("io.mockk:mockk:1.13.5")
     }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
