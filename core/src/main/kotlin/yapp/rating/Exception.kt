82a2ee105aed95744e38790868ae67388fe5e904
HoYeon Lee <comforest@naver.com>
2023-05-08 19:14:27 +0900

    feat: Auth Core Service

--- core/src/main/kotlin/yapp/rating/Exception.kt
+++ core/src/main/kotlin/yapp/rating/Exception.kt
@@ -4,4 +4,8 @@ import java.lang.RuntimeException
 
 sealed class YappRatingException(msg: String, cause: Throwable? = null) : RuntimeException(msg, cause)
 
-object InvalidTokenException : YappRatingException("유효하지 않는 토큰 입니다.")
+sealed class LoginException(msg: String, cause: Throwable? = null) : YappRatingException(msg, cause)
+
+class SocialLoginException(cause: Throwable? = null): LoginException("로그인이 실패했습니다.", cause)
+object InvalidTokenException : LoginException("유효하지 않는 토큰 입니다.")
+
