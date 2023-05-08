234a314f9813e786d91b2d520de2c281a073fbcf
HoYeon Lee <comforest@naver.com>
2023-05-08 22:46:26 +0900

    style: ktlint

--- core/src/main/kotlin/yapp/rating/Exception.kt
+++ core/src/main/kotlin/yapp/rating/Exception.kt
@@ -6,6 +6,5 @@ sealed class YappRatingException(msg: String, cause: Throwable? = null) : Runtim
 
 sealed class LoginException(msg: String, cause: Throwable? = null) : YappRatingException(msg, cause)
 
-class SocialLoginException(cause: Throwable? = null): LoginException("로그인이 실패했습니다.", cause)
+class SocialLoginException(cause: Throwable? = null) : LoginException("로그인이 실패했습니다.", cause)
 object InvalidTokenException : LoginException("유효하지 않는 토큰 입니다.")
-
