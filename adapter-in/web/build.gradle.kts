75a5f1eef0987a4b012f6fe28ef30001371b7f32
HoYeon Lee <comforest@naver.com>
2023-05-11 21:47:11 +0900

    feat: swagger generate

--- adapter-in/web/build.gradle.kts
+++ adapter-in/web/build.gradle.kts
@@ -1,9 +1,11 @@
 import groovy.lang.Closure
 import io.swagger.v3.oas.models.servers.Server
+import org.hidetake.gradle.swagger.generator.GenerateSwaggerUI
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("com.epages.restdocs-api-spec") version "0.18.0"
+    id("org.hidetake.swagger.generator") version "2.19.2"
 }
 
 dependencies {
@@ -17,17 +19,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.0")
     testImplementation("com.epages:restdocs-api-spec-mockmvc:0.18.0")
+    swaggerCodegen("io.swagger.codegen.v3:swagger-codegen-cli:3.0.42")
+    swaggerUI("org.webjars:swagger-ui:4.18.2")
 }
 
 tasks {
     withType<Jar> { enabled = false }
     withType<BootJar> {
         enabled = true
-//        mainClass.set("yapp.rating.boardgame.WebApplicationKt")
+        dependsOn("copySwaggerUI")
     }
-}
 
-tasks {
+    val generateSwaggerUIPrefix = "Api"
     openapi3 {
         setServers(
             listOf(
@@ -40,11 +43,27 @@ tasks {
         version = "0.1.0"
         format = "yaml"
     }
-
     postman {
         title = "My API"
         version = "0.1.0"
         baseUrl = "https://localhost:8080"
+    }
+    swaggerSources {
+        create(generateSwaggerUIPrefix).apply {
+            setInputFile(file("${project.buildDir}/api-spec/openapi3.yaml"))
+        }
+    }
+    withType<GenerateSwaggerUI> {
+        dependsOn("openapi3")
+    }
+    register<Copy>("copySwaggerUI") {
+        dependsOn("generateSwaggerUI$generateSwaggerUIPrefix")
+
+        val generateSwaggerUISampleTask =
+            this@tasks.named<GenerateSwaggerUI>("generateSwaggerUI$generateSwaggerUIPrefix").get()
+
+        from("${generateSwaggerUISampleTask.outputDir}")
+        into("src/main/resources/static/swagger")
     }
 }
 
