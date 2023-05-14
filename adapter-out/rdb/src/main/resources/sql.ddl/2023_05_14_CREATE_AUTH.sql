7461dd7a285e16279b271f817657de66945f076c
HoYeon Lee <comforest@naver.com>
2023-05-14 22:13:16 +0900

    style: updatedAt 으로 통일

--- adapter-out/rdb/src/main/resources/sql.ddl/2023_05_14_CREATE_AUTH.sql
+++ adapter-out/rdb/src/main/resources/sql.ddl/2023_05_14_CREATE_AUTH.sql
@@ -4,7 +4,7 @@ create table auth_social
     social_id   varchar(50) NOT NULL,
     user_id     bigint      NOT NULL,
     created_at  datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY auth_social (social_id, social_type)
 );
 
