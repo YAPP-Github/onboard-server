c3d3dd190892c13137b920c924aa59cf73d586a3
HoYeon Lee <comforest@naver.com>
2023-05-30 23:16:12 +0900

    fix: 사용하지 않는 package 제거

--- adapter-in/web/src/main/kotlin/com/yapp/bol/WebApplication.kt
+++ adapter-in/web/src/main/kotlin/com/yapp/bol/WebApplication.kt
@@ -3,8 +3,8 @@ package com.yapp.bol
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["yapp.rating"])
-open class WebApplication
+@SpringBootApplication
+class WebApplication
 
 fun main(args: Array<String>) {
     runApplication<WebApplication>(*args)
--- adapter-in/web/src/test/kotlin/yapp/rating/base/OpenApiTag.kt
+++
@@ -1,7 +0,0 @@
-package yapp.rating.base
-
-enum class OpenApiTag(
-    val value: String
-) {
-    TEST("test"),
-}
