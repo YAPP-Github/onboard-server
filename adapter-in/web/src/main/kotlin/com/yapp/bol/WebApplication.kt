cca08790dd4fae79e1cef0de5d1bdccca99e7442
HoYeon Lee <comforest@naver.com>
2023-06-03 00:48:29 +0900

    BOL-45 fix: SpringBootApplication 추가

--- adapter-in/web/src/main/kotlin/com/yapp/bol/WebApplication.kt
+++ adapter-in/web/src/main/kotlin/com/yapp/bol/WebApplication.kt
@@ -3,6 +3,7 @@ package com.yapp.bol
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@SpringBootApplication
 class WebApplication
 
 fun main(args: Array<String>) {
