6fa648524a8e9b0e3927f0884292d329ae61fe8c
HoYeon Lee <comforest@naver.com>
2023-06-04 12:53:33 +0900

    BOL-45 fix: Exception Handler에 status 추가

--- domain/src/main/kotlin/com/yapp/bol/Exception.kt
+++ domain/src/main/kotlin/com/yapp/bol/Exception.kt
@@ -4,14 +4,16 @@ import java.lang.RuntimeException
 
 sealed class BolRatingException(
     val code: String,
+    val status: Int,
     override val message: String,
     override val cause: Throwable? = null
 ) : RuntimeException(message, cause)
 
-sealed class LoginException(code: String, msg: String, cause: Throwable? = null) : BolRatingException(code, msg, cause)
+sealed class LoginException(code: String, msg: String, cause: Throwable? = null) : BolRatingException(code, 400, msg, cause)
 
 class SocialLoginFailedException(cause: Throwable? = null) : LoginException("Auth001", "로그인이 실패했습니다.", cause)
 object InvalidTokenException : LoginException("Auth002", "유효하지 않는 토큰 입니다.")
 object ExpiredTokenException : LoginException("Auth003", "만료된 토큰 입니다.")
 
-object InvalidRequestException : BolRatingException("Bol001", "유효하지 않은 요청입니다.")
+object InvalidRequestException : BolRatingException("BOL001", 400, "유효하지 않은 요청입니다.")
+object IllegalFileStateException : BolRatingException("BOL002", 500, "요청한 파일의 Status가 올바르지 않습니다.")
