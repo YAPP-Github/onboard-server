9b1221024c5cdd94e50bf2a37ab47c75d984dc67
SeungYoung Oh <seung-00@naver.com>
2023-06-09 01:13:36 +0900

    BOL-35 feat: MemberEntity 추가

---
+++ adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/member/MemberEntity.kt
@@ -0,0 +1,36 @@
+package com.yapp.bol.group.member
+
+import com.yapp.bol.AuditingEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "member")
+class MemberEntity(id: Long = 0, userId: Long? = null, role: MemberRole, nickname: String) : AuditingEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", nullable = false)
+    val id: Long = id
+
+    @Column(name = "users_id")
+    val userId: Long? = userId
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    val role: MemberRole = role
+
+    @Column(name = "nickname")
+    val nickname: String = nickname
+
+    @Column(name = "deleted")
+    val deleted: Boolean = false
+
+    @Column(name = "group_id")
+    val groupId: Long? = null
+}
