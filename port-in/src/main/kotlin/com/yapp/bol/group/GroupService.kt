5458cfb69da1301826ba4c5427a18914695045d8
SeungYoung Oh <seung-00@naver.com>
2023-06-09 01:28:57 +0900

    BOL-35 feat: group port-in 추가

---
+++ port-in/src/main/kotlin/com/yapp/bol/group/GroupService.kt
@@ -0,0 +1,9 @@
+package com.yapp.bol.group
+
+import com.yapp.bol.group.dto.CreateGroupDto
+
+interface GroupService {
+    fun createGroup(
+        createGroupDto: CreateGroupDto
+    ): Group
+}
---
+++ port-in/src/main/kotlin/com/yapp/bol/group/dto/CreateGroupDto.kt
@@ -0,0 +1,10 @@
+package com.yapp.bol.group.dto
+
+data class CreateGroupDto(
+    val name: String,
+    val description: String,
+    val organization: String,
+    val profileImageUrl: String?,
+    val ownerId: Long,
+    val nickname: String,
+)
