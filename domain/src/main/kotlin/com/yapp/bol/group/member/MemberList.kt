5b87d6ddcbe3f400ba9a8976c665e68b2becc791
SeungYoung Oh <seung-00@naver.com>
2023-06-08 23:20:22 +0900

    BOL-35 그룹 생성- 도메인 객체 추가 (#34)
    * BOL-35 Group 도메인 추가
    
    * BOL-35 Member, Members 도메인 추가
    
    * BOL-35 refactor: 불필요한 생성자 메서드 제거
    
    * BOL-35 refactor: club -> organization 으로 네이밍 변경
    
    * BOL-35 refactor: Members 수정
    
    * BOL-35 refactor: Group 코드 수정
    
    * BOL-35 refactor: MemberRole 추가
    
    * BOL-35 refactor: Members -> MemberList 로 변경
    
    * BOL-35 refactor: 불필요한 생성자 메서드 제거

---
+++ domain/src/main/kotlin/com/yapp/bol/group/member/MemberList.kt
@@ -0,0 +1,21 @@
+package com.yapp.bol.group.member
+
+class MemberList private constructor(private val members: MutableList<Member>) {
+    fun toList(): List<Member> {
+        return members.toList()
+    }
+
+    fun add(member: Member) {
+        members.add(member)
+    }
+
+    companion object {
+        fun of(owner: Member): MemberList {
+            if (!owner.isOwner()) {
+                throw IllegalArgumentException("그룹 생성자는 OWNER 역할이어야 합니다.")
+            }
+
+            return MemberList(mutableListOf(owner))
+        }
+    }
+}
