34ed02f8dab08031502612ee6e062cf51ae449ce
SeungYoung Oh <seung-00@naver.com>
2023-06-09 01:11:24 +0900

    BOL-35 feat: GroupEntity 추가

--- adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/GroupEntity.kt
+++ adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/GroupEntity.kt
@@ -1,3 +1,5 @@
+    @OneToMany(mappedBy = "groupId", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)    val members: List<MemberEntity> = members
+
 package com.yapp.bol.group
 
 import com.yapp.bol.AuditingEntity
@@ -8,7 +10,6 @@ import com.yapp.bol.group.member.toEntity
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
