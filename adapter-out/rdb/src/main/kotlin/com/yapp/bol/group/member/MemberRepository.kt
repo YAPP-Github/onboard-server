68df3dc8226f2d1633a5109d25bb3020ef3eb14d
SeungYoung Oh <seung-00@naver.com>
2023-06-09 15:59:35 +0900

    BOL-35 refactor: 코드 위치 변경 및 자잘한 수정

--- adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/member/MemberRepository.kt
+++ adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/member/MemberRepository.kt
@@ -7,3 +7,19 @@ import org.springframework.stereotype.Repository
 internal interface MemberRepository : JpaRepository<MemberEntity, Long> {
     fun findByNickname(nickname: String): MemberEntity?
 }
+
+fun MemberEntity.toDomain(): Member {
+    return Member(
+        id = id,
+        userId = userId,
+        role = role,
+        nickname = nickname,
+    )
+}
+
+fun Member.toEntity(): MemberEntity = MemberEntity(
+    id = this.id,
+    userId = this.userId,
+    role = this.role,
+    nickname = this.nickname
+)
