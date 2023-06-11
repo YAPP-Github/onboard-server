82e851832efe1da098c0d2de1786e36e8c4fcd99
SeungYoung Oh <seung-00@naver.com>
2023-06-11 22:25:42 +0900

    feat: 그룹 도메인, 엔티티에서 멤버 도메인, 엔티티 분리

--- domain/src/main/kotlin/com/yapp/bol/group/member/Member.kt
+++ domain/src/main/kotlin/com/yapp/bol/group/member/Member.kt
@@ -7,6 +7,7 @@ class Member(
     val userId: Long? = null,
     val role: MemberRole, // FIXME: 스프링 시큐리티 추가 전 임시 값
     val nickname: String,
+    val groupId: Long,
 ) {
     init {
         if (nickname.length > MAX_NICKNAME_LENGTH) {
@@ -19,12 +20,13 @@ class Member(
     companion object {
         const val MAX_NICKNAME_LENGTH = 6
 
-        fun createOwner(userId: Long, nickname: String): Member {
+        fun createOwner(userId: Long, nickname: String, groupId: Long): Member {
             return Member(
                 id = 0,
                 userId = userId,
                 role = MemberRole.OWNER,
                 nickname = nickname,
+                groupId = groupId,
             )
         }
     }
--- domain/src/test/kotlin/com/yapp/bol/group/GroupTest.kt
+++ domain/src/test/kotlin/com/yapp/bol/group/GroupTest.kt
@@ -3,7 +3,6 @@ package com.yapp.bol.group
 import com.yapp.bol.InvalidGroupDescriptionException
 import com.yapp.bol.InvalidGroupNameException
 import com.yapp.bol.InvalidGroupOrganizationException
-import com.yapp.bol.group.member.Member
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -11,10 +10,8 @@ import io.kotest.matchers.types.shouldBeInstanceOf
 
 class GroupTest : FunSpec() {
     init {
-        val owner = Member.createOwner(1, "nick")
-
         test("그룹 생성") {
-            val group = Group.of("name", "description", "organization", owner)
+            val group = Group.of("name", "description", "organization")
 
             group.shouldBeInstanceOf<Group>()
         }
@@ -23,7 +20,7 @@ class GroupTest : FunSpec() {
             val name = "x".repeat(Group.MAX_NAME_LENGTH + 1)
 
             shouldThrow<InvalidGroupNameException> {
-                Group.of(name, "description", "organization", owner)
+                Group.of(name, "description", "organization")
             }
         }
 
@@ -31,7 +28,7 @@ class GroupTest : FunSpec() {
             val description = "x".repeat(Group.MAX_DESCRIPTION_LENGTH + 1)
 
             shouldThrow<InvalidGroupDescriptionException> {
-                Group.of("name", description, "organization", owner)
+                Group.of("name", description, "organization")
             }
         }
 
@@ -39,18 +36,18 @@ class GroupTest : FunSpec() {
             val organization = "x".repeat(Group.MAX_ORGANIZATION_LENGTH + 1)
 
             shouldThrow<InvalidGroupOrganizationException> {
-                Group.of("name", "description", organization, owner)
+                Group.of("name", "description", organization)
             }
         }
 
         test("그룹 프로필 이미지 URL 이 비었다면 디폴트 이미지로 설정되어야 한다") {
-            val group = Group.of("name", "description", "organization", owner)
+            val group = Group.of("name", "description", "organization")
 
             group.profileImageUrl shouldBe Group.DEFAULT_PROFILE_IMAGE_URL
         }
 
         test("accessCode 가 자동으로 생성된다") {
-            val group = Group.of("name", "description", "organization", owner)
+            val group = Group.of("name", "description", "organization")
 
             group.accessCode.shouldBeInstanceOf<String>()
             group.accessCode.length shouldBe Group.ACCESS_CODE_LENGTH
