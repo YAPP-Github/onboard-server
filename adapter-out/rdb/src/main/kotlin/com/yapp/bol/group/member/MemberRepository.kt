4093a4144c49b5b3ac2452f609c3efa980a286a4
SeungYoung Oh <seung-00@naver.com>
2023-06-11 22:45:54 +0900

    feat: Group, Member port, adapter out 수정

--- adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/member/MemberRepository.kt
+++ adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/member/MemberRepository.kt
@@ -1,9 +1,11 @@
 package com.yapp.bol.group.member
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 internal interface MemberRepository : JpaRepository<MemberEntity, Long> {
     fun findByNickname(nickname: String): MemberEntity?
+
+    fun findByGroupId(groupId: Long): List<MemberEntity>
+
+    fun findByNicknameAndGroupId(nickname: String, groupId: Long): MemberEntity?
 }
