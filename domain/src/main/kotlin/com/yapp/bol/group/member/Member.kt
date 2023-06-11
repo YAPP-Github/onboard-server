5feb7118052e04fcd0a72c9e221726f24151aaf7
SeungYoung Oh <seung-00@naver.com>
2023-06-11 22:24:57 +0900

    feat: Group, Member 도메인 수정

--- domain/src/main/kotlin/com/yapp/bol/group/member/Member.kt
+++ domain/src/main/kotlin/com/yapp/bol/group/member/Member.kt
@@ -3,7 +3,7 @@ package com.yapp.bol.group.member
 import com.yapp.bol.InvalidMemberNicknameException
 
 class Member(
-    val id: Long,
+    val id: Long = 0,
     val userId: Long? = null,
     val role: MemberRole, // FIXME: 스프링 시큐리티 추가 전 임시 값
     val nickname: String,
@@ -22,7 +22,6 @@ class Member(
 
         fun createOwner(userId: Long, nickname: String, groupId: Long): Member {
             return Member(
-                id = 0,
                 userId = userId,
                 role = MemberRole.OWNER,
                 nickname = nickname,
