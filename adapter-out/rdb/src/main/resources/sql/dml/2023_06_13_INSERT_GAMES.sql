ae6dba13ad09c76bf41428494d6d2a8de6e2d5cb
Lee Ho Yean <comforest@naver.com>
2023-06-17 14:42:26 +0900

    BOL-92 2.  Game - AdpaterOut (#41)
    * BOL-92 feat: SQL 문 생성
    
    * BOL-92 feat: Game Jpa 클래스
    
    * BOL-92 fix: Entity 에서도 value class 사용하기

---
+++ adapter-out/rdb/src/main/resources/sql/dml/2023_06_13_INSERT_GAMES.sql
@@ -0,0 +1,13 @@
+INSERT INTO game(name, min_member, max_member, rank_type, img_id)
+values ("스플랜더", 2, 4, "SCORE_HIGH", 1),
+       ("테라포밍 마스", 1, 4, "SCORE_HIGH", 1),
+       ("가이아", 1, 4, "SCORE_HIGH", 1),
+       ("아그리콜라", 1, 4, "SCORE_HIGH", 1),
+       ("테라 미스티카", 1, 4, "SCORE_HIGH", 1),
+       ("윙스펜", 1, 4, "SCORE_HIGH", 1),
+       ("스플랜더 - Dummy", 2, 4, "SCORE_HIGH", 1),
+       ("테라포밍 마스 - Dummy", 1, 4, "SCORE_HIGH", 1),
+       ("가이아 - Dummy", 1, 4, "SCORE_HIGH", 1),
+       ("아그리콜라 - Dummy", 1, 4, "SCORE_HIGH", 1),
+       ("테라 미스티카 - Dummy", 1, 4, "SCORE_HIGH", 1),
+       ("윙스펜 - Dummy", 1, 4, "SCORE_HIGH", 1);
