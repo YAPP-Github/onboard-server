70e158b70598247ba612c130969a2c2af650afad
Lee Ho Yean <comforest@naver.com>
2023-07-08 23:33:45 +0900

    BOL-153 feat: Auth Api 에 남은 온보딩 보여주기 (#68)

--- adapter-in/web/src/test/kotlin/com/yapp/bol/auth/AuthControllerTest.kt
+++ adapter-in/web/src/test/kotlin/com/yapp/bol/auth/AuthControllerTest.kt
@@ -1,26 +1,35 @@
 package com.yapp.bol.auth
 
 import com.yapp.bol.auth.dto.LoginRequest
+import com.yapp.bol.base.ARRAY
 import com.yapp.bol.base.ControllerTest
 import com.yapp.bol.base.ENUM
 import com.yapp.bol.base.OpenApiTag
 import com.yapp.bol.base.STRING
+import com.yapp.bol.onboarding.OnboardingService
+import com.yapp.bol.onboarding.OnboardingType
 import io.mockk.every
 import io.mockk.mockk
 import java.time.LocalDateTime
 
 class AuthControllerTest : ControllerTest() {
     private val authService: AuthService = mockk()
-    override val controller = AuthController(authService)
+    private val onboardingService: OnboardingService = mockk()
+    override val controller = AuthController(authService, onboardingService)
 
     init {
         test("POST /v1/auth/login") {
             val request = LoginRequest(LoginType.KAKAO_ACCESS_TOKEN, "Token")
+            val userId = UserId(123L)
             val authToken = AuthToken(
-                Token("ACCESS_TOKEN", UserId(123L), LocalDateTime.now()),
-                Token("REFRESH_TOKEN", UserId(123L), LocalDateTime.now())
+                Token("ACCESS_TOKEN", userId, LocalDateTime.now()),
+                Token("REFRESH_TOKEN", userId, LocalDateTime.now())
             )
             every { authService.login(any(), any()) } returns authToken
+            every { onboardingService.getRemainOnboarding(userId) } returns listOf(
+                OnboardingType.TERMS,
+                OnboardingType.NICKNAME,
+            )
 
             post("/v1/auth/login", request) {}
                 .isStatus(200)
@@ -33,6 +42,7 @@ class AuthControllerTest : ControllerTest() {
                     responseFields(
                         "accessToken" type STRING means "Access 토큰",
                         "refreshToken" type STRING means "Refresh 토큰" isOptional true,
+                        "onboarding" type ARRAY means "남은 온보딩 단계 ${OnboardingType.values().toList()}",
                     )
                 )
         }
