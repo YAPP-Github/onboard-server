caaa276bf36bc0e19116bd39ac13e105abd59c02
SeungYoung Oh <seung-00@naver.com>
2023-07-25 00:22:20 +0900

    BOL-95 매치 기록 > adapter-in 추가 (#77)
    * BOL-95 feat: GameMember Repository 추가
    
    * BOL-95 feat: Season Repository 추가
    
    * BOL-95 feat: MatchMember Repository 추가
    
    * BOL-95 feat: Match Repository 추가
    
    * BOL-95 feat: GameMember port out 추가
    
    * BOL-95 feat: Season port out 추가
    
    * BOL-95 feat: Match port out 추가
    
    * BOL-95 feat: Season 도메인 추가
    
    * BOL-95 feat: Match 도메인 추가
    
    * BOL-95 feat: MatchMember 도메인 추가
    
    * BOL-95 feat: GameMember 도메인 추가
    
    * BOL-95 feat: GameMemberService 추가
    
    * BOL-95 feat: CreateMatchDto 추가
    
    * BOL-95 feat: MatchService 추가
    
    * BOL-95 feat: SeasonService 추가
    
    * BOL-95 feat: MatchWithMatchMemberList 추가
    
    * BOL-95 test: GameMemberTest 추가
    
    * BOL-95 feat: GameMemberServiceImpl 추가
    
    * BOL-95 feat: MatchServiceImpl 추가
    
    * BOL-95 feat: SeasonServiceImpl 추가
    
    * BOL-95 feat: MatchMember 예외 클래스 추가
    
    * BOL-95 feat: CreateMatchRequest 추가
    
    * BOL-95 feat: MatchController 추가
    
    * BOL-95 feat: MatchControllerTest 추가
    
    * BOL-95 feat: CreateMatchRequest 수정
    
    * BOL-95 test: MatchControllerTest 수정

---
+++ adapter-in/web/src/main/kotlin/com/yapp/bol/match/dto/CreateMatchRequest.kt
@@ -0,0 +1,33 @@
+package com.yapp.bol.match.dto
+
+import com.yapp.bol.game.GameId
+import com.yapp.bol.group.GroupId
+import com.yapp.bol.group.member.MemberId
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+data class MatchMemberRequest(
+    val memberId: Long,
+    val score: Int,
+    val ranking: Int,
+)
+
+data class CreateMatchRequest(
+    val gameId: Long,
+    val groupId: Long,
+    val matchedDate: String,
+    val matchMembers: List<MatchMemberRequest>
+)
+
+internal fun MatchMemberRequest.toDto() = CreateMatchMemberDto(
+    memberId = MemberId(this.memberId),
+    score = this.score,
+    ranking = this.ranking,
+)
+
+internal fun CreateMatchRequest.toDto() = CreateMatchDto(
+    gameId = GameId(this.gameId),
+    groupId = GroupId(this.groupId),
+    matchedDate = LocalDateTime.parse(this.matchedDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+    matchMembers = this.matchMembers.map { it.toDto() }
+)
---
+++ adapter-in/web/src/test/kotlin/com/yapp/bol/match/MatchControllerTest.kt
@@ -0,0 +1,138 @@
+package com.yapp.bol.match
+
+import com.yapp.bol.auth.UserId
+import com.yapp.bol.base.ARRAY
+import com.yapp.bol.base.ControllerTest
+import com.yapp.bol.base.NUMBER
+import com.yapp.bol.base.OpenApiTag
+import com.yapp.bol.base.STRING
+import com.yapp.bol.game.GameId
+import com.yapp.bol.group.GroupId
+import com.yapp.bol.group.member.MemberId
+import com.yapp.bol.match.dto.CreateMatchRequest
+import com.yapp.bol.match.dto.MatchMemberRequest
+import com.yapp.bol.match.dto.MatchWithMatchMemberList
+import com.yapp.bol.match.member.MatchMember
+import com.yapp.bol.match.member.MatchMemberId
+import com.yapp.bol.season.Season
+import com.yapp.bol.season.SeasonId
+import io.mockk.every
+import io.mockk.mockk
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class MatchControllerTest : ControllerTest() {
+    private val matchService: MatchService = mockk()
+    override val controller = MatchController(matchService)
+
+    init {
+        test("매치 기록하기") {
+            val matchMembers = listOf(
+                MatchMemberRequest(
+                    memberId = 1,
+                    score = 1,
+                    ranking = 1,
+                ),
+                MatchMemberRequest(
+                    memberId = 2,
+                    score = 2,
+                    ranking = 2,
+                ),
+                MatchMemberRequest(
+                    memberId = 3,
+                    score = 3,
+                    ranking = 3,
+                ),
+                MatchMemberRequest(
+                    memberId = 4,
+                    score = 4,
+                    ranking = 4,
+                ),
+            )
+
+            val request = CreateMatchRequest(
+                gameId = 1,
+                groupId = 1,
+                matchedDate = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                matchMembers = matchMembers
+            )
+
+            every {
+                matchService.createMatch(any())
+            } returns MATCH_WITH_MATCH_MEMBER_LIST
+
+            post("/v1/match", request) {
+                authorizationHeader(UserId(1))
+            }
+                .isStatus(200)
+                .makeDocument(
+                    DocumentInfo(identifier = "group/{method-name}", tag = OpenApiTag.MATCH),
+                    requestFields(
+                        "gameId" type NUMBER means "game id",
+                        "groupId" type NUMBER means "group id",
+                        "matchedDate" type STRING means "match 시간",
+                        "matchMembers" type ARRAY means "match 멤버 목록",
+                        "matchMembers[].memberId" type NUMBER means "member id",
+                        "matchMembers[].score" type NUMBER means "점수",
+                        "matchMembers[].ranking" type NUMBER means "순위",
+                    ),
+                )
+        }
+    }
+
+    companion object {
+        private val SEASON = Season(
+            id = SeasonId(1),
+            groupId = GroupId(1),
+        )
+
+        private val MATCH = Match(
+            id = MatchId(1),
+            gameId = GameId(1),
+            groupId = GroupId(1),
+            matchedDate = LocalDateTime.now(),
+            memberCount = 1,
+            season = SEASON
+        )
+
+        private val MATCH_MEMBERS = listOf(
+            MatchMember(
+                id = MatchMemberId(1),
+                matchId = MATCH.id,
+                memberId = MemberId(1),
+                score = 1,
+                ranking = 1,
+                previousScore = 0
+            ),
+            MatchMember(
+                id = MatchMemberId(2),
+                matchId = MATCH.id,
+                memberId = MemberId(2),
+                score = 2,
+                ranking = 2,
+                previousScore = 0
+            ),
+            MatchMember(
+                id = MatchMemberId(3),
+                matchId = MATCH.id,
+                memberId = MemberId(3),
+                score = 3,
+                ranking = 3,
+                previousScore = 0
+            ),
+            MatchMember(
+                id = MatchMemberId(4),
+                matchId = MATCH.id,
+                memberId = MemberId(4),
+                score = 4,
+                ranking = 4,
+                previousScore = 0
+            ),
+        )
+
+        private val MATCH_WITH_MATCH_MEMBER_LIST = MatchWithMatchMemberList(
+            match = MATCH,
+            matchMembers = MATCH_MEMBERS
+        )
+    }
+}
