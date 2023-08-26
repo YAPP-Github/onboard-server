9a142c690763910b0a8cc48e9ec7f6b664ed2fc4
SeungYoung Oh <seung-00@naver.com>
2023-08-26 17:33:30 +0900

    health check API 에 phase 체크용 기능 추가 (#91)
    * feat: health 체크용 API 에 phase 검사 기능 추가
    
    * test: TestControllerTest 에 phase 검사 추가
    
    * refactor: 불필요한 코드 제거

--- adapter-in/web/src/main/kotlin/com/yapp/bol/TestController.kt
+++ adapter-in/web/src/main/kotlin/com/yapp/bol/TestController.kt
@@ -1,6 +1,6 @@
 package com.yapp.bol
 
-import com.yapp.bol.utils.logger
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,17 +8,12 @@ import org.springframework.web.bind.annotation.RestController
 // TODO: Delete 테스트 코드 전용 클래스
 @RestController
 @RequestMapping("/v1/test")
-class TestController {
-    private val logger = logger()
-
+class TestController(private val environment: Environment) {
     @GetMapping
     fun testGet(): TestResponse {
-        logger.error("error")
-        logger.warn("warn")
-        logger.info("info")
-        logger.debug("debug")
-        logger.trace("trace")
-        return TestResponse("Good!")
+        val activeProfiles = environment.activeProfiles.joinToString(", ")
+
+        return TestResponse("Good! You're running in the $activeProfiles phase.")
     }
 
     data class TestResponse(
